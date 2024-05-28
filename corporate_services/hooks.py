edf16cc8860d04944fbcccb649d3c36daa6dbc2a
Brian Amolo <railamolo@gmail.com>
2024-05-28 10:56:09 +0300

    sample doc event

--- corporate_services/hooks.py
+++ corporate_services/hooks.py
@@ -122,13 +122,16 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-# 	}
-# }
+doc_events = {
+	# "*": {
+	# 	"on_update": "method",
+	# 	"on_cancel": "method",
+	# 	"on_trash": "method"
+	# },
+    "Travel Request":{
+        "on_submit": "corporate_services.api.notifications.alert_supervisor"
+    }
+}
 
 # Scheduled Tasks
 # ---------------
