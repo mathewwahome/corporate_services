44248a1abf31f445e4149be1421221572876036a
mathewwahome <methewwahome@gmail.com>
2024-07-09 15:23:48 +0300

    permissions_test

--- corporate_services/api/timesheet/timesheet_generation_export.py
+++ corporate_services/api/timesheet/timesheet_generation_export.py
@@ -2,7 +2,7 @@ import frappe
 import csv
 from io import StringIO
 from frappe.utils.file_manager import save_file
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 @frappe.whitelist()
 def timesheet_generation_export(docname):
@@ -10,20 +10,36 @@ def timesheet_generation_export(docname):
         doc = frappe.get_doc('Timesheet Submission', docname)
         
         employee = doc.employee
-        start_date = doc.start_date
-        end_date = doc.end_date
-        month = doc.month
         employee_name = frappe.db.get_value('Employee', employee, 'employee_name')
+        month_name = doc.month
+        
+        current_year = datetime.now().year
+        current_month = datetime.strptime(month_name, "%B").month
+
+        if current_month == 1:
+            previous_year = current_year - 1
+            previous_month = 12
+        else:
+            previous_year = current_year
+            previous_month = current_month - 1
+
+        start_date = datetime(previous_year, previous_month, 28).date()
+        end_date = datetime(current_year, current_month, 28).date()   
         
         dates = []
-        current_date = start_date
-        while current_date <= end_date:
-            dates.append(current_date.strftime('%d'))
-            current_date += timedelta(days=1)
+        while start_date <= end_date:
+            dates.append(start_date.strftime('%d'))
+            start_date += timedelta(days=1)
 
+        #get the user linked to the specific employee
+        user =  frappe.db.get_value('User', employee, 'user')
+        # get the projects allocated to the specific employee
+        projects = frappe.db.get_value('Project', user, 'project_name')
+        
+             
         data = [
-            ["Actvity Type", "Tasks"] + dates,
-            ["Project"],
+            ["Projects", "Tasks"] + dates,
+            [projects],
             ["Meetings"],
             ["Proposals"],
             ["Recurring Tasks"],
@@ -37,7 +53,7 @@ def timesheet_generation_export(docname):
         csv_writer.writerows(data)
 
         csv_content = output.getvalue().encode('utf-8')
-        file_name = f"{employee_name}_{month}_{docname}.xlsx" # csv
+        file_name = f"{employee_name}_{current_month}_{docname}.csv"
 
         file_url = save_file(file_name, csv_content, "Timesheet Submission", docname, is_private=0)
 
