1e94979430c570ff90cb8c5734800e719134ea3e
mathewwahome <methewwahome@gmail.com>
2024-08-18 06:14:27 +0300

    Automatically submit all timesheets in the child table of a Timesheet Submission document when the workflow state is set to "Approved by Finance"

--- corporate_services/hooks.py
+++ corporate_services/hooks.py
@@ -147,6 +147,7 @@ on_update_map = {
     "Asset Requisition": "corporate_services.api.notification.asset_requisition.alert",
     "Timesheet Submission":"corporate_services.api.notification.timesheet.alert",
     "Timesheet Submission":"corporate_services.api.timesheet.before_workflow_action.before_workflow_action_timesheet_submission",
+    "Timesheet Submission":"corporate_services.api.timesheet.finance_timesheet_submission.finance_timesheet_submission",
     
     "Project":"corporate_services.api.notification.project.project_manager.alert"
 }
