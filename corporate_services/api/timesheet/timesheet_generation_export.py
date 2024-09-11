88f315215d7352aea15fea4cc52fff214a4f889f
mathewwahome <methewwahome@gmail.com>
2024-09-11 11:05:59 +0300

    Update on the timesheet ui - adding background color on to it to identify the weekends

--- corporate_services/api/timesheet/timesheet_generation_export.py
+++ corporate_services/api/timesheet/timesheet_generation_export.py
@@ -2,6 +2,7 @@ import frappe
 from frappe.utils.file_manager import save_file
 from datetime import datetime, timedelta
 from openpyxl import Workbook
+from openpyxl.styles import PatternFill
 from io import BytesIO
 
 @frappe.whitelist()
@@ -27,10 +28,12 @@ def timesheet_generation_export(docname):
             previous_month = month - 1
 
         start_date = datetime(previous_year, previous_month, 29).date()
-        end_date = datetime(year, month, 28).date()   
+        end_date = datetime(year, month, 28).date()
         
+        days = []
         dates = []
         while start_date <= end_date:
+            days.append(start_date.strftime('%A'))
             dates.append(start_date.strftime('%d'))
             start_date += timedelta(days=1)
         
@@ -46,7 +49,8 @@ def timesheet_generation_export(docname):
                         projects_list.append([project['project_name']])
                         projects_list.append([])
                         projects_list.append([])
-
+                            
+        week = ["",""] + days
         header = ["Projects", "Tasks"] + dates
         
         activity_types = frappe.get_all('Activity Type', fields=['name'])
@@ -59,6 +63,7 @@ def timesheet_generation_export(docname):
         ws = wb.active
         ws.title = "Timesheet"
 
+        ws.append(week)
         ws.append(header)
         
         for row in projects_list:
@@ -66,13 +71,22 @@ def timesheet_generation_export(docname):
         
         for row in additional_rows:
             ws.append(row)
-        
+
+        weekend_fill = PatternFill(start_color="87CEEB", end_color="87CEEB", fill_type="solid")
+
+        max_row = ws.max_row
+
+        for col_num, day in enumerate(days, start=3):
+            ws.cell(row=2, column=col_num).fill = weekend_fill
+            if day in ('Saturday', 'Sunday'):
+                for row_num in range(1, max_row + 1):
+                    ws.cell(row=row_num, column=col_num).fill = weekend_fill
+
         output = BytesIO()
         wb.save(output)
         output.seek(0)
         
         file_name = f"{employee_name}-{month_name}{year}-Timesheet.xlsx"
-
 
         file_url = save_file(file_name, output.read(), "Timesheet Submission", docname, is_private=0)
 
