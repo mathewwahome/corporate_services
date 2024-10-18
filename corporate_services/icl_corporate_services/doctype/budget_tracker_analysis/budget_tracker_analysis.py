604766534263cd7a6f2ddff3daa9281c35c556bc
mathewwahome <methewwahome@gmail.com>
2024-10-18 09:15:21 +0300

    Revert "owner update, consultany and general"
    This reverts commit 2aaa60b23217d144e0783904c705f7adf5d8e15a.

--- corporate_services/icl_corporate_services/doctype/budget_tracker_analysis/budget_tracker_analysis.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, IntelliSOFT Consulting and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class BudgetTrackerAnalysis(Document):
-	pass
