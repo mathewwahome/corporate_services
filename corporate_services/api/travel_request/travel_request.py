4e2eff8ef771fc0df4d31ca1921c94d2e2245499
mathewwahome <methewwahome@gmail.com>
2024-11-08 08:54:37 +0300

    Travel request per diem rates permission issues update

--- corporate_services/api/travel_request/travel_request.py
+++ corporate_services/api/travel_request/travel_request.py
@@ -2,10 +2,10 @@ import frappe
 
 @frappe.whitelist()
 def get_per_diem_rates(place):
-    if not frappe.has_permission("Per Diem Rates Child Table", "read"):
+    if not frappe.has_permission("Per Diem Rates", "read"):
         frappe.throw("You do not have permission to access this resource.")
     
-    return frappe.get_all("Per Diem Rates Child Table", 
+    return frappe.get_all("Per Diem Rates", 
                       filters={"place_of_travel": place}, 
                       fields=["place_of_travel", "accommodation_max", "accommodation_min", 
                               "meal_incidentals_expenses_max", "meal_incidentals_expenses_min"])
