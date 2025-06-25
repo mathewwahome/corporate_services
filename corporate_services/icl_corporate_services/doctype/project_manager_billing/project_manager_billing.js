f9f9c9550e136a2d912a5f1fb09335a6252defb1
mathewwahome <methewwahome@gmail.com>
2025-06-25 20:53:14 +0300

    Introduction of the project manager billing form

---
+++ corporate_services/icl_corporate_services/doctype/project_manager_billing/project_manager_billing.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, IntelliSOFT Consulting and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Project Manager Billing", {
+// 	refresh(frm) {
+
+// 	},
+// });
