e6fdef2772e7cab383bb92008c59cbf2074c5c5e
mathewwahome <methewwahome@gmail.com>
2026-02-05 12:35:30 +0300

    Staff Requisition Update

--- corporate_services/api/notification/staff_requisition/staff_requisition.py
+++ corporate_services/api/notification/staff_requisition/staff_requisition.py
@@ -13,80 +13,113 @@ def send_email(recipients, subject, message, pdf_content, doc_name):
         header=("Staff Requisition", "text/html")
     )
 
-def generate_message(doc, employee_name, email_type):
+def get_user_full_name(email):
+    """Get the full name of a user from their email"""
+    return frappe.get_value('User', email, 'full_name') or email
+
+def generate_message(doc, employee_name, email_type, recipient_email):
     doctype_url = get_url_to_form(doc.doctype, doc.name)
+    recipient_name = get_user_full_name(recipient_email)
+    
     messages = {
         "submitted_to_hr": """
-            Dear HR,<br><br>
+            Dear {},<br><br>
             {}, {} has been submitted for your review. You can view the details <a href="{}">here</a>.<br><br>
             Kind regards,<br>
             System
-        """.format(employee_name, doc.doctype, doctype_url),
+        """.format(recipient_name, employee_name, doc.doctype, doctype_url),
 
         "submitted_to_finance": """
-            Dear Finance,<br><br>
+            Dear {},<br><br>
             {}, {} has been reviewed and approved by HR. You can view the details <a href="{}">here</a>.<br><br>
             Kind regards,<br>
             HR
-        """.format(employee_name, doc.doctype, doctype_url),
+        """.format(recipient_name, employee_name, doc.doctype, doctype_url),
+        
+        "submitted_to_ceo": """
+            Dear {},<br><br>
+            {}, {} has been reviewed and approved by Finance. You can view the details <a href="{}">here</a>.<br><br>
+            Kind regards,<br>
+            Finance
+        """.format(recipient_name, employee_name, doc.doctype, doctype_url),
+        
+        "approved_by_ceo": """
+            Dear {},<br><br>
+            {}, {} has been approved by the CEO. You can view the details <a href="{}">here</a>.<br><br>
+            Kind regards,<br>
+            CEO
+        """.format(recipient_name, employee_name, doc.doctype, doctype_url)
     }
     return messages[email_type]
+
+def get_users_with_role(role):
+    """Get email addresses of all users with a specific role"""
+    users = frappe.get_all('Has Role', filters={'role': role}, fields=['parent'])
+    return [frappe.get_value('User', user.parent, 'email') for user in users]
+
+def add_approval_if_not_exists(doc, title):
+    """Add approval entry if it doesn't already exist"""
+    approval_exists = any(row.title == title for row in doc.staff_requisition_approval)
+    
+    if not approval_exists:
+        doc.append('staff_requisition_approval', {
+            'title': title,
+            'employee_name': frappe.session.user,
+            'datetime': now_datetime()
+        })
+        doc.flags.in_alert = True
+        doc.save(ignore_permissions=True)
 
 def alert(doc, method):
     if doc.flags.in_alert:
         return
     
-    if doc.workflow_state in ["Submitted to HR", "Submitted to Finance"]:
-        # Get requestor employee details
-        requestor_id = doc.requestor
-        requestor = frappe.get_doc("Employee", requestor_id)
-        requestor_name = requestor.employee_name
-        
-        print_format = "Standard"
-        pdf_content = frappe.get_print(doc.doctype, doc.name, print_format, as_pdf=True)
-
-        if doc.workflow_state == "Submitted to HR":
-            hr_managers = frappe.get_all('Has Role', filters={'role': 'HR Manager'}, fields=['parent'])
-            hr_manager_emails = [frappe.get_value('User', hr_manager.parent, 'email') for hr_manager in hr_managers]
+    if doc.workflow_state not in ["Submitted to HR", "Submitted to Finance", "Submitted to CEO", "Approved by CEO"]:
+        return
+    
+    requestor = frappe.get_doc("Employee", doc.requestor)
+    requestor_name = requestor.employee_name
+    
+    pdf_content = frappe.get_print(doc.doctype, doc.name, "Standard", as_pdf=True)
+    
+    workflow_config = {
+        "Submitted to HR": {
+            "recipients": get_users_with_role('HR Manager'),
+            "message_type": "submitted_to_hr",
+            "approval_title": "Requestor"
             
-            message_to_hr = generate_message(doc, requestor_name, "submitted_to_hr")
-            send_email(
-                recipients=hr_manager_emails,
-                subject=frappe._('Staff Requisition from {}'.format(requestor_name)),
-                message=message_to_hr,
-                pdf_content=pdf_content,
-                doc_name=doc.name
-            )
-          
-        elif doc.workflow_state == "Submitted to Finance":
-            # Check if HR approval already exists to prevent duplicate entries
-            hr_approval_exists = any(
-                row.title == 'HR' 
-                for row in doc.staff_requisition_approval
-            )
-            
-            if not hr_approval_exists:
-                # Add approval record to child table
-                doc.append('staff_requisition_approval', {
-                    'title': 'HR',
-                    'employee_name': frappe.session.user,
-                    'datetime': now_datetime()
-                })
-                
-                doc.flags.in_alert = True
-                doc.save(ignore_permissions=True)
-            
-            finance_team = frappe.get_all('Has Role', filters={'role': 'Finance'}, fields=['parent'])
-            finance_team_emails = [frappe.get_value('User', finance_manager.parent, 'email') for finance_manager in finance_team]
-            
-            message_to_finance = generate_message(doc, requestor_name, "submitted_to_finance")
-            send_email(
-                recipients=finance_team_emails,
-                subject=frappe._('Staff Requisition from {}'.format(requestor_name)),
-                message=message_to_finance,
-                pdf_content=pdf_content,
-                doc_name=doc.name
-            )
+        },
+        "Submitted to Finance": {
+            "recipients": get_users_with_role('Finance'),
+            "message_type": "submitted_to_finance",
+            "approval_title": "HR"
+        },
+        "Submitted to CEO": {
+            "recipients": get_users_with_role('CEO'),
+            "message_type": "submitted_to_ceo",
+            "approval_title": "Finance"
+        },
+        "Approved by CEO": {
+            "recipients": get_users_with_role('HR Manager'),
+            "message_type": "approved_by_ceo",
+            "approval_title": "CEO"
+        }
+    }
+    
+    config = workflow_config[doc.workflow_state]
+    
+    if "approval_title" in config:
+        add_approval_if_not_exists(doc, config["approval_title"])
+    
+    for recipient_email in config["recipients"]:
+        message = generate_message(doc, requestor_name, config["message_type"], recipient_email)
+        send_email(
+            recipients=[recipient_email],
+            subject=frappe._('Staff Requisition from {}'.format(requestor_name)),
+            message=message,
+            pdf_content=pdf_content,
+            doc_name=doc.name
+        )
 
 doc_events = {
     "Staff Requisition": {
